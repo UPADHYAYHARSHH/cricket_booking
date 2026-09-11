8bdcc59bd6b5c2101c4172bc9d1949c1ea33c127
UPADHYAYHARSHH <upadhyayh920@gmail.com>
2026-09-11 12:25:48 +0530

    fix: Fix 3px bottom overflow in VenueCard horizontal list

--- lib/user_booking/presentation/screens/ground_list/ground_list_screen.dart
+++ lib/user_booking/presentation/screens/ground_list/ground_list_screen.dart
@@ -456,7 +456,7 @@ class _GroundListScreenState extends State<GroundListScreen> {
               ),
             ),
             SizedBox(
-              height: 295,
+              height: 305,
               child: ListView.separated(
                 scrollDirection: Axis.horizontal,
                 padding: const EdgeInsets.symmetric(horizontal: 16),
